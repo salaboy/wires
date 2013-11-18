f1b0f2102262aee212468673302be8ba458d6021
Horacio Antar <horacioantar@gmail.com>
2013-11-18 17:11:32 -0300

    Changing circleFactory and shapeFactory

--- wires-webapp/src/main/java/org/kie/wires/client/factoryShapes/CircleFactory.java
+++ wires-webapp/src/main/java/org/kie/wires/client/factoryShapes/CircleFactory.java
@@ -1,10 +1,12 @@
 package org.kie.wires.client.factoryShapes;
 
+import com.emitrom.lienzo.client.core.shape.Circle;
 import com.emitrom.lienzo.client.core.shape.Group;
-import com.emitrom.lienzo.client.core.shape.Line;
 import com.emitrom.lienzo.client.core.shape.Shape;
 
-public class CircleFactory extends ShapeFactory<Line> {
+public class CircleFactory extends ShapeFactory<Circle> {
+    
+    private static String description = "Circle";
 
     @Override
     public void drawBoundingBox(Group group) {
@@ -13,7 +15,7 @@ public class CircleFactory extends ShapeFactory<Line> {
     }
 
     @Override
-    public Shape<Line> drawShape() {
+    public Shape<Circle> drawShape() {
         // TODO Auto-generated method stub
         return null;
     }
@@ -25,9 +27,11 @@ public class CircleFactory extends ShapeFactory<Line> {
     }
 
     @Override
-    public void addHandlers(Shape<Line> shape, Group group) {
+    public void addHandlers(Shape<Circle> shape, Group group) {
         // TODO Auto-generated method stub
         
     }
+
+   
 
 }
