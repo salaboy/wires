24534450e86ef10e9a5beb9d796f85ddf6e6399a
hernsys <horacioantar@gmail.com>
2013-11-26 13:23:05 -0300

    The bounding box and the Lines and Rectangles are dynamics in the stencil

--- wires-webapp/src/main/java/org/kie/wires/client/connectors/ConnectorsGroup.java
+++ wires-webapp/src/main/java/org/kie/wires/client/connectors/ConnectorsGroup.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 
 import org.kie.wires.client.events.ShapeAddEvent;
 import org.kie.wires.client.factoryShapes.ShapeBuilder;
+import org.kie.wires.client.factoryShapes.ShapeFactoryUtil;
 import org.kie.wires.client.factoryShapes.ShapeType;
 
 import com.emitrom.lienzo.client.core.shape.Group;
@@ -22,11 +23,10 @@ public class ConnectorsGroup extends Composite {
 
     @Inject
     private ShapeBuilder shapeBuilder;
-    
-    
+
     @PostConstruct
     public void init() {
-        LienzoPanel panel = new LienzoPanel(200, 300);
+        LienzoPanel panel = new LienzoPanel(ShapeFactoryUtil.WIDTH_PANEL, ShapeFactoryUtil.HEIGHT_PANEL);
         initWidget(panel);
         Layer layer = new Layer();
         panel.add(layer);
@@ -34,8 +34,26 @@ public class ConnectorsGroup extends Composite {
         group1.setX(0).setY(5);
         layer.add(group1);
         shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
+        shapeBuilder.newShape(group1, ShapeType.LINE, panel, shapeAddEvent);
         layer.draw();
     }
-
 
 }
