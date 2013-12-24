9d6a2f9918fae6aa4e83794b028e8d7abf293b9a
hernsys <horacioantar@gmail.com>
2013-12-24 10:55:07 -0300

    Adding options(view and delete) in the layers panel

--- wires-webapp/src/main/java/org/kie/wires/client/factoryLayers/LayerBuilder.java
+++ wires-webapp/src/main/java/org/kie/wires/client/factoryLayers/LayerBuilder.java
@@ -2,6 +2,7 @@ package org.kie.wires.client.factoryLayers;
 
 import com.emitrom.lienzo.client.core.shape.Circle;
 import com.emitrom.lienzo.client.core.shape.Group;
+import com.emitrom.lienzo.client.core.shape.Layer;
 import com.emitrom.lienzo.client.core.shape.Line;
 import com.emitrom.lienzo.client.core.shape.Rectangle;
 import com.emitrom.lienzo.client.core.shape.Shape;
@@ -14,17 +15,17 @@ public class LayerBuilder extends Composite {
     public LayerBuilder() {
     }
 
-    public LayerBuilder(Group group, final Shape shape, LienzoPanel panel, int accountLayers) {
-        this.newLayer(group, shape, panel, accountLayers);
+    public LayerBuilder(Group group, final Shape shape, LienzoPanel panel, Layer layer, int accountLayers) {
+        this.newLayer(group, shape, panel, accountLayers, layer);
     }
 
-    public void newLayer(Group group, final Shape shape, LienzoPanel panel, int accountLayers) {
+    public void newLayer(Group group, final Shape shape, LienzoPanel panel, int accountLayers, Layer layer) {
         if (shape instanceof Line) {
-            new LayerLineFactory(group, panel, accountLayers);
+            new LayerLineFactory(group, panel, accountLayers, layer);
         } else if (shape instanceof Rectangle) {
-            new LayerRectangleFactory(group, panel, accountLayers);
+            new LayerRectangleFactory(group, panel, accountLayers, layer);
         } else if (shape instanceof Circle) {
-            new LayerCircleFactory(group, panel, accountLayers);
+            new LayerCircleFactory(group, panel, accountLayers, layer);
         }
     }
 
